a4c650dd5d3b04b5702b84902ce9056adb88197b
Salman Muin Kayser Chishti <13schishti@Gmail.com>
2019-07-13 13:31:46 +0100

    added second middleware

---
+++ EchoBot1/Middleware/Middleware2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EchoBot1.Middleware
+{
+    public class Middleware2 : IMiddleware
+    {
+        public async Task OnTurnAsync(ITurnContext turnContext, NextDelegate next)
+        {
+            await turnContext.SendActivityAsync($"[Middleware2] {turnContext.Activity.Type}/OnTurn/Before");
+
+            if(turnContext.Activity.Type == ActivityTypes.Message && turnContext.Activity.Text == "secret password")
+            {
+                await next();
+            }
+
+            await turnContext.SendActivityAsync($"[Middleware2] {turnContext.Activity.Type}/OnTurn/After");
+        }
+    }
+}
