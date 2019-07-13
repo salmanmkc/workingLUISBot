aa794c25fd3c01f0d0675ccad90121640d86c06c
Salman Muin Kayser Chishti <13schishti@Gmail.com>
2019-07-13 13:27:37 +0100

    adding first middleware

---
+++ EchoBot1/Middleware/Middleware1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EchoBot1.Middleware
+{
+    public class Middleware1 : IMiddleware
+    {
+        public async Task OnTurnAsync(ITurnContext turnContext, NextDelegate next, CancellationToken cancellationToken = default)
+        {
+            await turnContext.SendActivityAsync($"[Middleware#1 {turnContext.Activity.Type}/OnTurn/Before");
+            await next();
+            await turnContext.SendActivityAsync($"[Middleware#2] {turnContext.Activity.Type}/OnTurn/After");
+        }
+    }
+}
